30ca184cff913bd9d86a829aa54ca56cadc7af51
andra <andramariamates2@gmail.com>
2026-03-18 22:59:16 +0200

    Add favicon

--- src/main/java/ro/andramates/moot/config/SecurityConfig.java
+++ src/main/java/ro/andramates/moot/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login","/register","/css/**","/js/**", "/images/**", "/error/**").permitAll()
+                        .requestMatchers("/", "/login","/register","/css/**","/js/**", "/images/**", "/error/**", "/favicon.ico").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user/**").hasAnyRole("USER","ADMIN")
                         .anyRequest().authenticated()
